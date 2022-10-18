a12f697ea2dfde091edbc321fd23d4af31502df3
Djielo <business.jlo68@gmail.com>
2022-10-18 21:02:25 +0200

    suite 4

--- src/index.jsx
+++ src/index.jsx
@@ -8,12 +8,12 @@ const ModalEmployeeCreated = ({ image, message }) => {
   }
 
   return (
-    <div className="jlo_modalContainer" onClick={hide}>
-      <div className="jlo_overlay"></div>
-      <div className="jlo_modal">
-        <button className="jlo_modalClose">X</button>
-        <img className="jlo_modalImage" src={image} alt="Employee Created" />
-        <p className="jlo_modalText">{msg}</p>
+    <div className="jielo_modalContainer" onClick={hide}>
+      <div className="jielo_overlay"></div>
+      <div className="jielo_modal">
+        <button className="jielo_modalClose">X</button>
+        <img className="jielo_modalImage" src={image} alt="Employee Created" />
+        <p className="jielo_modalText">{msg}</p>
       </div>
     </div>
   );
